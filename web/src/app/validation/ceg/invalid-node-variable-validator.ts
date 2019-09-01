e094dbae24dfe03fa234c0b1c1748089ece95583
Maximilian Junker <maximilian.junker@qualicen.de>
2019-09-01 17:26:07 +0200

    make validators catch invalid node names in CEGs

--- web/src/app/validation/ceg/invalid-node-variable-validator.ts
+++ web/src/app/validation/ceg/invalid-node-variable-validator.ts
@@ -10,17 +10,39 @@ import { ValidNameValidator } from '../valid-name-validator';
 import { ValidationUtil } from '../validation-util';
 import { ValidationErrorSeverity } from '../validation-error-severity';
 
-@Validator(CEGNode)
+/** 
+ * Detects nodes in a model that have a character in the variable name that is not allowed for
+ * element names. As from the variable name Specmate contstruct Testparameters with the same name, this 
+ * would cause problems.
+ */
+@Validator(CEGModel)
 export class InvalidNodeVariableValidator extends ElementValidatorBase<CEGNode> {
 
     public validate(element: CEGNode, contents: IContainer[]): ValidationResult {
+
+        let nodes: CEGNode[] =
+            contents.filter((element: IContainer) => Type.is(element, CEGNode)).map((element: IContainer) => element as CEGNode);
+        let invalidNodes: CEGNode[] = [];
+        for(let node of nodes){
+            if(!this.isValidNode(node)){
+                invalidNodes.push(node);
+            }
+        }
+        if(invalidNodes.length>0){
+            let message = ValidationMessage.ERROR_INVALID_VARIABLE;
+            return new ValidationResult(message, false, invalidNodes, ValidationErrorSeverity.SAVE_DISABLED);
+       }
+        
+    }
+
+    /* Checks if a CEG node has a valid variable name */
+    private isValidNode(element: CEGNode): boolean {
         if (element === undefined || element.variable === undefined) {
-            return ValidationResult.VALID;
+            return true;
         }
         if (!ValidationUtil.isValidName(element.variable)) {
-            let message = ValidationMessage.ERROR_INVALID_VARIABLE;
-            return new ValidationResult(message, false, [element], ValidationErrorSeverity.SAVE_DISABLED);
+            return false;
         }
-        return ValidationResult.VALID;
-    }
+        return true;
+    } 
 }

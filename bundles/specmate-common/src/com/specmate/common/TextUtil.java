13b85a89323fff2e47bfacdc6b6414ca77579235
wehrlean <49229000+wehrlean@users.noreply.github.com>
2019-09-03 14:44:29 +0200

    Format class

--- bundles/specmate-common/src/com/specmate/common/TextUtil.java
+++ bundles/specmate-common/src/com/specmate/common/TextUtil.java
@@ -3,17 +3,18 @@ package com.specmate.common;
 import java.util.Set;
 
 public class TextUtil {
-	
-	
+
 	/**
-	 * Compares, if two strings contains the same words. Case-sensitivity is irrelevant.
+	 * Compares, if two strings contains the same words. Case-sensitivity is
+	 * irrelevant.
+	 * 
 	 * @param textA
 	 * @param textB
 	 * @return
 	 */
 	public static boolean textsContainSameWords(String textA, String textB) {
-		if(textA.equalsIgnoreCase(textB)) { 
-			return true; 
+		if (textA.equalsIgnoreCase(textB)) {
+			return true;
 		}
 		String textALower = textA.toLowerCase();
 		String textBLower = textB.toLowerCase();
@@ -21,7 +22,7 @@ public class TextUtil {
 		String[] arrayB = textBLower.split("\\s+");
 		Set<String> setA = Set.of(arrayA);
 		Set<String> setB = Set.of(arrayB);
-		
+
 		return setA.equals(setB);
 	}
 }

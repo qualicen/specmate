d8288e8f64ddaa30a1bd74322e44ac398e2e1694
wehrlean <49229000+wehrlean@users.noreply.github.com>
2019-09-05 09:29:26 +0200

    Add comment

--- bundles/specmate-common/src/com/specmate/common/UUIDUtil.java
+++ bundles/specmate-common/src/com/specmate/common/UUIDUtil.java
@@ -3,6 +3,8 @@ package com.specmate.common;
 import java.util.UUID;
 
 public class UUIDUtil {
+	
+	// Generates a new id (we hope we do not get duplicates. We do not check for that.)
 	public static String generateUUID() {
 		UUID uuid = UUID.randomUUID();
         return uuid.toString();

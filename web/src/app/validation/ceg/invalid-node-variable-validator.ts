849aca947612a45e1e580833360ce106c5013ca6
extern-junker-maximilian <extern.junker_maximilian@allianz.de>
2019-09-06 11:26:04 +0200

    fix validator to consider nodes of models

--- web/src/app/validation/ceg/invalid-node-variable-validator.ts
+++ web/src/app/validation/ceg/invalid-node-variable-validator.ts
@@ -1,12 +1,10 @@
 import { CEGModel } from '../../model/CEGModel';
 import { CEGNode } from '../../model/CEGNode';
 import { IContainer } from '../../model/IContainer';
-import { Type } from '../../util/type';
 import { ElementValidatorBase } from '../element-validator-base';
 import { ValidationMessage } from '../validation-message';
 import { ValidationResult } from '../validation-result';
 import { Validator } from '../validator-decorator';
-import { ValidNameValidator } from '../valid-name-validator';
 import { ValidationUtil } from '../validation-util';
 import { ValidationErrorSeverity } from '../validation-error-severity';
 
@@ -15,25 +13,17 @@ import { ValidationErrorSeverity } from '../validation-error-severity';
  * element names. As from the variable name Specmate contstruct Testparameters with the same name, this 
  * would cause problems.
  */
-@Validator(CEGModel)
+@Validator(CEGNode)
 export class InvalidNodeVariableValidator extends ElementValidatorBase<CEGNode> {
 
     public validate(element: CEGNode, contents: IContainer[]): ValidationResult {
 
-        let nodes: CEGNode[] =
-            contents.filter((element: IContainer) => Type.is(element, CEGNode)).map((element: IContainer) => element as CEGNode);
-        let invalidNodes: CEGNode[] = [];
-        for(let node of nodes){
-            if(!this.isValidNode(node)){
-                invalidNodes.push(node);
-            }
-        }
-        if(invalidNodes.length>0){
+        if (!this.isValidNode(element)) {
             let message = ValidationMessage.ERROR_INVALID_VARIABLE;
-            return new ValidationResult(message, false, invalidNodes, ValidationErrorSeverity.SAVE_DISABLED);
-       }
-       return ValidationResult.VALID;
-        
+            return new ValidationResult(message, false, [element], ValidationErrorSeverity.SAVE_DISABLED);
+        }
+        return ValidationResult.VALID;
+
     }
 
     /* Checks if a CEG node has a valid variable name */
@@ -45,5 +35,5 @@ export class InvalidNodeVariableValidator extends ElementValidatorBase<CEGNode> 
             return false;
         }
         return true;
-    } 
+    }
 }

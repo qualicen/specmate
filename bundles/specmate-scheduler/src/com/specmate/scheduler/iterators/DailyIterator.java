8590c9eeb759ea0716618d66b883393b8bfd2880
Tobias Springer <tobias.springer@tum.de>
2019-09-09 15:38:42 +0200

    Remove: unused import

--- bundles/specmate-scheduler/src/com/specmate/scheduler/iterators/DailyIterator.java
+++ bundles/specmate-scheduler/src/com/specmate/scheduler/iterators/DailyIterator.java
@@ -1,8 +1,6 @@
 package com.specmate.scheduler.iterators;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;

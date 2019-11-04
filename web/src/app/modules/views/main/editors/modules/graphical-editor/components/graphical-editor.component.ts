515a628b2b39bf8fb568e1b2683ddfeec1420615
Daedo <public@dominik-spies.de>
2019-11-04 17:34:08 +0100

    Show Grid (#34)
    * Show Grid

--- web/src/app/modules/views/main/editors/modules/graphical-editor/components/graphical-editor.component.ts
+++ web/src/app/modules/views/main/editors/modules/graphical-editor/components/graphical-editor.component.ts
@@ -301,6 +301,7 @@ export class GraphicalEditor {
       this.undoManager.clear();
       this.validationService.validateCurrent();
     }
+    this.initGraphicalGrid();
   }
 
   private async initCEGModel(): Promise<void> {
@@ -335,6 +336,121 @@ export class GraphicalEditor {
       return mx.mxGraph.prototype.getTooltipForCell.bind(this.graph)(cell);
     };
     this.vertexPrivider.initCEGRenderer(this.graph);
+  }
+
+  private repaintGrid: () => void;
+  private forceRedraw = false;
+  private gridColor = '#8c8c8c';
+
+  private async initGraphicalGrid(): Promise<void> {
+    this.graph.setPanning(true);
+    const graph = this.graph;
+    (function() {
+      const canvas = document.createElement('canvas');
+      canvas.style.position = 'absolute';
+      canvas.style.top = '0px';
+      canvas.style.left = '0px';
+      canvas.style.zIndex = '-1';
+      graph.container.appendChild(canvas);
+      graph.container.style.zIndex = '1';
+      let ctx = canvas.getContext('2d');
+
+      // Modify event filtering to accept canvas as container
+      let mxGraphViewIsContainerEvent = mx.mxGraphView.prototype.isContainerEvent;
+      mx.mxGraphView.prototype.isContainerEvent = function(evt) {
+        return mxGraphViewIsContainerEvent.apply(this, arguments) ||
+        mx.mxEvent.getSource(evt) == canvas;
+      };
+
+      let s = 0;
+      let gs = 0;
+      let tr = new mx.mxPoint();
+      let w = 0;
+      let h = 0;
+      this.repaintGrid = function() {
+        if (ctx != null) {
+          let bounds = graph.getGraphBounds();
+          let width = Math.max(bounds.x + bounds.width, graph.container.clientWidth);
+          let height = Math.max(bounds.y + bounds.height, graph.container.clientHeight);
+          let sizeChanged = width != w || height != h;
+
+          if (graph.view.scale != s || graph.view.translate.x != tr.x || graph.view.translate.y != tr.y ||
+            gs != graph.gridSize || sizeChanged || this.forceRedraw) {
+            this.forceRedraw = false;
+            tr = graph.view.translate.clone();
+            s = graph.view.scale;
+            gs = graph.gridSize;
+            w = width;
+            h = height;
+
+            // Clears the background if required
+            if (!sizeChanged) {
+              ctx.clearRect(0, 0, w, h);
+            } else {
+              canvas.setAttribute('width', w + '');
+              canvas.setAttribute('height', h + '');
+            }
+
+            let tx = tr.x * s;
+            let ty = tr.y * s;
+
+            // Sets the distance of the grid lines in pixels
+            let minStepping = graph.gridSize;
+            let stepping = minStepping * s;
+
+            if (stepping < minStepping) {
+              let count = Math.round(Math.ceil(minStepping / stepping) / 2) * 2;
+              stepping = count * stepping;
+            }
+
+            let xs = Math.floor((0 - tx) / stepping) * stepping + tx;
+            let xe = Math.ceil(w / stepping) * stepping;
+            let ys = Math.floor((0 - ty) / stepping) * stepping + ty;
+            let ye = Math.ceil(h / stepping) * stepping;
+
+            xe += Math.ceil(stepping);
+            ye += Math.ceil(stepping);
+
+            let ixs = Math.round(xs);
+            let ixe = Math.round(xe);
+            let iys = Math.round(ys);
+            let iye = Math.round(ye);
+
+            // Draws the actual grid
+            if (this.isGridShown) {
+              ctx.strokeStyle = this.gridColor;
+              ctx.beginPath();
+
+              for (let x = xs; x <= xe; x += stepping) {
+                x = Math.round((x - tx) / stepping) * stepping + tx;
+                let ix = Math.round(x);
+
+                ctx.moveTo(ix + 0.5, iys + 0.5);
+                ctx.lineTo(ix + 0.5, iye + 0.5);
+              }
+
+              for (let y = ys; y <= ye; y += stepping) {
+                y = Math.round((y - ty) / stepping) * stepping + ty;
+                let iy = Math.round(y);
+
+                ctx.moveTo(ixs + 0.5, iy + 0.5);
+                ctx.lineTo(ixe + 0.5, iy + 0.5);
+              }
+
+              ctx.closePath();
+              ctx.stroke();
+            }
+          }
+        }
+      }.bind(this);
+
+      const repaint = this.repaintGrid;
+      let mxGraphViewValidateBackground = mx.mxGraphView.prototype.validateBackground;
+      mx.mxGraphView.prototype.validateBackground = function() {
+        mxGraphViewValidateBackground.apply(this, arguments);
+        repaint();
+      };
+    }).bind(this)();
   }
 
   private updateValidities(): void {
@@ -449,10 +565,14 @@ export class GraphicalEditor {
 
   public showGrid(): void {
     this.isGridShown = true;
+    this.forceRedraw = true;
+    this.repaintGrid();
   }
 
   public hideGrid(): void {
     this.isGridShown = false;
+    this.forceRedraw = true;
+    this.repaintGrid();
   }
 
   public get connections(): IContainer[] {

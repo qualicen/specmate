87339761317523e1df4daa2b79199c6b1a4b09e7
wehrlean <49229000+wehrlean@users.noreply.github.com>
2020-01-09 14:31:37 +0100

    Set center of zoom to top left corner

--- web/src/app/modules/views/main/editors/modules/graphical-editor/components/graphical-editor.component.ts
+++ web/src/app/modules/views/main/editors/modules/graphical-editor/components/graphical-editor.component.ts
@@ -136,6 +136,7 @@ export class GraphicalEditor {
     mx.mxConnectionHandler.prototype.connectImage = new mx.mxImage('/assets/img/editor-tools/connector.png', 16, 16);
     mx.mxGraph.prototype.warningImage = new mx.mxImage('/assets/img/editor-tools/error_red.png', 19, 19);
     mx.mxGraphHandler.prototype['guidesEnabled'] = true;
+    mx.mxGraph.prototype.centerZoom = false;
 
     mx.mxEvent.disableContextMenu(this.graphContainerElement.nativeElement);
 

267cce2a4564eca55cadb53691e5882c9b7b5244
wehrlean <49229000+wehrlean@users.noreply.github.com>
2020-01-28 13:29:43 +0100

    Use npm module and update warning text

--- web/src/app/modules/actions/modules/browser-version-checker/components/browser-version-checker.component.ts
+++ web/src/app/modules/actions/modules/browser-version-checker/components/browser-version-checker.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Config } from '../../../../../config/config';
+import { detect } from 'detect-browser';
 
 @Component({
   moduleId: module.id.toString(),
@@ -8,54 +10,33 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['browser-version-checker.component.css']
 })
 
-
-
 export class BrowserVersionChecker {
 
-  private unsupportedBrowser = [
-    { browser: 'IE', minVersion: 11 },
-    { browser: 'MSIE', minVersion: 10 },
-    { browser: 'Chrome', minVersion: 42 },
-    { browser: 'Firefox', minVersion: 44 },
-    { browser: 'Edge', minVersion: 14 },
-    { browser: 'Safari', minVersion: 9 },
-    { browser: 'Opera', minVersion: 29 }];
-
   constructor(private translate: TranslateService) {
-
   }
 
   public get browserOutdated(): boolean {
-    let currentBrowser = this.getBrowser().split('!!');
-    for (let i = 0; i < this.unsupportedBrowser.length; i++) {
-      const element = this.unsupportedBrowser[i];
-      if (element.browser === currentBrowser[0]) {
-        if (element.minVersion >= parseInt(currentBrowser[1])) {
-          return true;
+    let currentBrowser = detect();
+    if (currentBrowser === null || currentBrowser === undefined || currentBrowser.name === null || currentBrowser.name === undefined) {
+      return true;
+    }
+
+    let supportedBrowsers = Config.SUPPORTED_BROWSERS;
+    for (let i = 0; i < supportedBrowsers.length; i++) {
+      const element = supportedBrowsers[i];
+      if (currentBrowser.name === element.browser) {
+        if (parseInt(currentBrowser.version.split('.')[0]) >= element.minVersion) {
+          return false;
         }
       }
     }
-    return false;
+    return true;
   }
 
-  // Based on https://stackoverflow.com/a/5918791
-  private getBrowser() {
-    let ua = navigator.userAgent, tem,
-      M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if (/trident/i.test(M[1])) {
-      tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-      return 'IE' + '!!' + (tem[1] || '');
-    }
-    if (M[1] === 'Chrome') {
-      tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
-      if (tem != null) {
-        return tem.slice(1).join('!!').replace('OPR', 'Opera');
-      }
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((tem = ua.match(/version\/(\d+)/i)) != null) {
-      M.splice(1, 1, tem[1]);
-    }
-    return M.join('!!');
+  private get supportedBrowsers(): String {
+    let supportedBrowsers = Config.SUPPORTED_BROWSERS;
+    let chrome = 'Google Chrome ' + supportedBrowsers.find(element => element.browser === 'chrome').minVersion + '+';
+    let firefox = 'Mozilla Firefox ' + supportedBrowsers.find(element => element.browser === 'firefox').minVersion + '+';
+    return  chrome + ' or ' + firefox;
   }
 }

98d28cb9d4cad393bd3e891b47e8bc0f52ef149b
Maximilian Junker <maximilian.junker@qualicen.de>
2020-02-04 17:27:49 +0100

    wip: basic jira export

--- bundles/specmate-jira-connector/src/com/specmate/connectors/jira/internal/services/JiraExportService.java
+++ bundles/specmate-jira-connector/src/com/specmate/connectors/jira/internal/services/JiraExportService.java
@@ -1,9 +1,11 @@
 package com.specmate.connectors.jira.internal.services;
 
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.StreamSupport;
 
 import org.osgi.service.component.annotations.Activate;
@@ -13,16 +15,20 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.log.LogService;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.BasicIssue;
 import com.atlassian.jira.rest.client.api.domain.IssueType;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
+import com.atlassian.util.concurrent.Promise;
 import com.specmate.common.exception.SpecmateException;
 import com.specmate.common.exception.SpecmateInternalException;
 import com.specmate.connectors.api.IExportService;
 import com.specmate.connectors.api.IProjectConfigService;
 import com.specmate.connectors.jira.config.JiraConnectorConfig;
 import com.specmate.model.administration.ErrorCode;
+import com.specmate.model.support.util.SpecmateEcoreUtil;
 import com.specmate.model.testspecification.TestProcedure;
+import com.specmate.model.testspecification.TestStep;
 
 @Component(immediate = true, service = IExportService.class, configurationPid = JiraConnectorConfig.EXPORTER_PID, configurationPolicy = ConfigurationPolicy.REQUIRE)
 public class JiraExportService implements IExportService {
@@ -62,8 +68,56 @@ public class JiraExportService implements IExportService {
 	public void export(TestProcedure testProcedure) throws SpecmateException {
 		IssueInputBuilder issueBuilder = new IssueInputBuilder(projectName, testType.getId());
 		issueBuilder.setSummary("Specmate Exported Test Procedure: " + testProcedure.getName());
+		StringBuilder builder = new StringBuilder();
+		builder.append("||Step||Name||Description||Expected Result||\n");
+		List<TestStep> steps = SpecmateEcoreUtil.pickInstancesOf(testProcedure.getContents(), TestStep.class);
+		steps.sort((s1, s2) -> Integer.compare(s1.getPosition(), s2.getPosition()));
+		int stepNum = 0;
+		for (TestStep step : steps) {
+			stepNum++;
+			builder.append("| " + stepNum + " | " + step.getName() + " | " + step.getDescription() + " | "
+					+ step.getExpectedOutcome() + " |\n");
+		}
+//		builder.append("</tbody></table> </div>");
+		issueBuilder.setDescription(builder.toString());
 		IssueInput issueInput = issueBuilder.build();
-		jiraClient.getIssueClient().createIssue(issueInput);
+		Promise<BasicIssue> result = jiraClient.getIssueClient().createIssue(issueInput);
+		BasicIssue createdIssue;
+		try {
+			createdIssue = result.get();
+			System.out.println(createdIssue);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		ArrayList<ComplexIssueInputFieldValue> steps = new ArrayList<ComplexIssueInputFieldValue>();
+//		for (TestStep step : SpecmateEcoreUtil.pickInstancesOf(testProcedure.getContents(), TestStep.class)) {
+//			Map<String, Object> stepValues = new HashMap<String, Object>();
+//			stepValues.put("step", step.getDescription());
+//			stepValues.put("result", step.getExpectedOutcome());
+//			stepValues.put("index", step.getPosition());
+//			steps.add(new ComplexIssueInputFieldValue(stepValues));
+//		}
+//		ComplexIssueInputFieldValue stepsFieldValue = ComplexIssueInputFieldValue.with("steps", steps);
+//		issueBuilder.setFieldValue("customfield_10004", stepsFieldValue);
+//		IssueInput issueInput = issueBuilder.build();
+//		Promise<BasicIssue> result = jiraClient.getIssueClient().createIssue(issueInput);
+//		BasicIssue createdIssue;
+//		try {
+//			createdIssue = result.get();
+//			System.out.println(createdIssue);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
 	}
 
 	@Override

471aa17f8306a1245a168ad77aa554e9f2456f7a
sebeder <23170307+sebeder@users.noreply.github.com>
2020-02-05 13:03:39 +0100

    Correcting debug version

--- dist/specmate.sh
+++ dist/specmate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-javas -version >/dev/null 2>&1
+java -version >/dev/null 2>&1
 if [ $? -eq 127 ]
 then
     echo "Java not found."
@@ -8,4 +8,4 @@ then
 fi
 
 echo "Running Specmate"
-#java -Djdk.crypto.KeyAgreement.legacyKDF=true -jar prod-specmate-all.jar --configurationFile specmate-config.properties
+java -Djdk.crypto.KeyAgreement.legacyKDF=true -jar prod-specmate-all.jar --configurationFile specmate-config.properties

3d0632e35fdc146cd172dec4f09364c695950a7c
Tobias Springer <tobias.springer@tum.de>
2020-03-04 13:17:38 +0100

    Add multiple test browsers

--- ui-tests/src/test/java/com/specmate/uitests/TestBase.java
+++ ui-tests/src/test/java/com/specmate/uitests/TestBase.java
@@ -73,8 +73,8 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
         LinkedList<String[]> browsers = new LinkedList<String[]>();
 
         browsers.add(new String[]{"Windows 10", "79.0", "Chrome", null, null});
-        //browsers.add(new String[]{"Windows 10", "14.14393", "MicrosoftEdge", null, null});
-        //browsers.add(new String[]{"Windows 10", "11.0", "internet explorer", null, null});
+        browsers.add(new String[]{"Windows 10", "44.18363", "MicrosoftEdge", null, null});
+        browsers.add(new String[]{"Windows 10", "73.0", "firefox", null, null});
         return browsers;
     }
 

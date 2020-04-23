b4ca4211d50c92ef95c9ecfb2683e30e7971d7f9
Tobias Springer <tobias.springer@qualicen.de>
2020-04-23 12:22:11 +0200

    Change id of project button

--- ui-tests/src/test/java/com/specmate/uitests/pagemodel/LoginElements.java
+++ ui-tests/src/test/java/com/specmate/uitests/pagemodel/LoginElements.java
@@ -65,7 +65,7 @@ public class LoginElements {
 	/**change project to <code>name</code> using the project dropdown*/
 	public void changeToProject(String name) {
 		driver.findElement(project).click();
-		driver.findElement(By.id("project-" + name)).click();
+		driver.findElement(By.id("project-")).click();
 	}
 	
 	public boolean isLoggedIn() {

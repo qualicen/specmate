97bc7c97b8b39a594d202523a7d6880c98442382
hauptmab <62144233+hauptmab@users.noreply.github.com>
2020-11-25 09:17:20 +0100

    cookies are set depending on http/https (#181)
    * cookies are set depending on http/https
    
    * cookies are set now based on `document.location.protocol`
    
    * Update cookies-service.ts
    
    Fix formatting
    
    Co-authored-by: sebeder <23170307+sebeder@users.noreply.github.com>

---
+++ web/src/app/modules/common/modules/cookies/services/cookies-service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
+
+@Injectable()
+export class CookiesService {
+
+  constructor(private ngxcookie: CookieService) { }
+
+  public setCookie(cookieName: string, cookieValue: any) {
+    let cookieOptions = {};
+
+    if (document.location.protocol == 'https:') {
+        cookieOptions = { sameSite: 'none', secure: true };
+    } else {
+        cookieOptions = { sameSite: 'lax'};
+    }
+
+    this.ngxcookie.putObject(cookieName, cookieValue, cookieOptions );
+  }
+
+  public getCookie(cookieName: string): string {
+    return this.ngxcookie.get(cookieName);
+  }
+
+  public getCookieObject (cookieName: string): object {
+      return this.ngxcookie.getObject(cookieName);
+  }
+
+  public removeCookie(cookieName: string): void {
+      this.ngxcookie.remove(cookieName);
+  }
+}

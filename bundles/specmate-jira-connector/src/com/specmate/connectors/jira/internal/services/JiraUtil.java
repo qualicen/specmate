e4f7752239df9e90570445ec01a4ff1696425524
Benedikt Hauptmann <benedikt.hauptmann@qualicen.de>
2021-01-12 18:08:33 +0100

    added function to get list of all (accessible) jira projects.

--- bundles/specmate-jira-connector/src/com/specmate/connectors/jira/internal/services/JiraUtil.java
+++ bundles/specmate-jira-connector/src/com/specmate/connectors/jira/internal/services/JiraUtil.java
@@ -3,12 +3,15 @@ package com.specmate.connectors.jira.internal.services;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.RestClientException;
+import com.atlassian.jira.rest.client.api.domain.BasicProject;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.specmate.common.exception.SpecmateAuthorizationException;
 import com.specmate.common.exception.SpecmateException;
@@ -28,6 +31,7 @@ public class JiraUtil {
 		try {
 			client = JiraUtil.createJiraRESTClient(url, username, password);
 			client.getProjectClient().getProject(project).claim();
+			
 		} catch (URISyntaxException e) {
 			throw new SpecmateAuthorizationException("Jira authentication failed.", e);
 		} catch (RestClientException e) {
@@ -47,6 +51,28 @@ public class JiraUtil {
 			}
 		}
 		return true;
+	}
+	
+	/**
+	 * Gets a list of all project-keys a given pair of credentials has access to.
+	 */
+	public static List<String> getProjects(String serverUrl, String username, String password)
+			throws SpecmateException {
+		JiraRestClient client = null;
+		try {
+
+			List<String> projectKeys = new ArrayList<>();
+			
+			client = JiraUtil.createJiraRESTClient(serverUrl, username, password);			
+			for (BasicProject project : client.getProjectClient().getAllProjects().claim()) {
+				projectKeys.add( project.getKey() );
+			}
+			
+			return projectKeys;						
+			
+		} catch (Exception e) {
+			throw new SpecmateAuthorizationException("Jira authentication failed.", e);
+		}
 	}
 
 	public static JSONObject stepToJson(TestStep step, boolean isCloud) {

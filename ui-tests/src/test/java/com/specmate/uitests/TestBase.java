3e26165fefac679f2c48592564f3932ce58c2801
sebeder <23170307+sebeder@users.noreply.github.com>
2021-02-02 09:23:04 +0100

    Update TestBase.java

--- ui-tests/src/test/java/com/specmate/uitests/TestBase.java
+++ ui-tests/src/test/java/com/specmate/uitests/TestBase.java
@@ -72,7 +72,7 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
     public static LinkedList<String[]> browsersStrings() {
         LinkedList<String[]> browsers = new LinkedList<String[]>();
 
-        browsers.add(new String[]{"Windows 10", "79.0", "Chrome", null, null});
+        browsers.add(new String[]{"Windows 10", "88.0", "Chrome", null, null});
         //browsers.add(new String[]{"Windows 10", "18.17763", "MicrosoftEdge", null, null});
         //browsers.add(new String[]{"Windows 10", "73.0", "firefox", null, null});
         return browsers;

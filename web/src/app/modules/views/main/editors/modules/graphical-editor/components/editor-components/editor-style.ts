e0c8c64aae411604cfebda167adcf74276659454
Sebastian Eder <23170307+sebeder@users.noreply.github.com>
2021-04-28 09:58:28 +0200

    Increase font size on edges

--- web/src/app/modules/views/main/editors/modules/graphical-editor/components/editor-components/editor-style.ts
+++ web/src/app/modules/views/main/editors/modules/graphical-editor/components/editor-components/editor-style.ts
@@ -156,5 +156,7 @@ export class EditorStyle {
         const edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
         edgeStyle[mx.mxConstants.STYLE_STROKECOLOR] = EditorStyle.EDGE_DIM_STYLE[mx.mxConstants.STYLE_STROKECOLOR];
         edgeStyle[mx.mxConstants.STYLE_STROKEWIDTH] = EditorStyle.EDGE_DIM_STYLE[mx.mxConstants.STYLE_STROKEWIDTH];
+        edgeStyle[mx.mxConstants.STYLE_FONTSIZE] = '18';
+        edgeStyle[mx.mxConstants.DEFAULT_FONTSIZE] = '18';
     }
 }

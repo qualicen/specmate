8dd76a8ac079fb2fadf4ea82c0af6c0efab6ef53
wehrlean <49229000+wehrlean@users.noreply.github.com>
2021-06-23 15:29:57 +0200

    TEST 5

--- ui-tests/src/test/java/com/specmate/uitests/ModelEditorTest.java
+++ ui-tests/src/test/java/com/specmate/uitests/ModelEditorTest.java
@@ -27,7 +27,7 @@ public class ModelEditorTest extends TestBase {
 	 */
 	@Test
 	public void verifyModelEditorTest() throws InvalidElementStateException {
-		// Test 4
+		// Test 5
 		Actions builder = new Actions(driver);
 
 		RequirementOverviewElements requirementOverview = new RequirementOverviewElements(driver);

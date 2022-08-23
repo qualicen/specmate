8c3b66495bd32473156103e87a500ffad083f7ef
sebeder <23170307+sebeder@users.noreply.github.com>
2022-08-23 12:05:35 +0200

    Enable headless mode

--- web/src/app/modules/navigation/modules/navigation-bar/components/navigation-bar.component.ts
+++ web/src/app/modules/navigation/modules/navigation-bar/components/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewControllerService } from 'src/app/modules/views/controller/modules/view-controller/services/view-controller.service';
 import { ENV } from '../../../../../../environments/environment';
 import { VERSION } from '../../../../../../environments/version';
 
@@ -8,7 +9,29 @@ import { VERSION } from '../../../../../../environments/version';
     templateUrl: 'navigation-bar.component.html',
     styleUrls: ['navigation-bar.component.css']
 })
-export class NavigationBar {
+export class NavigationBar implements OnInit {
+    public showVersion = true;
+    public showSearchBar = true;
+    public showProjectChooser = true;
+    public showOperationMonitor = true;
+    public showLanguageChooser = true;
+    public showDocumentationButton = true;
+    public showLogout = true;
+
+    constructor(private viewController: ViewControllerService) { }
+
+    ngOnInit(): void {
+        this.viewController.viewChanged.subscribe(() => {
+            this.showVersion = !this.viewController.isHeadless;
+            this.showSearchBar = !this.viewController.isHeadless;
+            this.showProjectChooser = !this.viewController.isHeadless;
+            //this.showOperationMonitor = !this.viewController.isHeadless;
+            //this.showLanguageChooser = !this.viewController.isHeadless;
+            //this.showDocumentationButton = !this.viewController.isHeadless;
+            this.showLogout = !this.viewController.isHeadless;
+        });
+    }
+
     public get version(): string {
         return VERSION.NUMBER + ' (' + ENV.TYPE + ')';
     }
